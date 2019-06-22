9a26189b31bb260683e6560898dcc31e9a9407da
Conjunctive <conjunctive@protonmail.com>
2019-06-21 17:05:11 -0700

    Updates

---
+++ precache-manifest.3a56291d147144b64968b3a62aaaa4e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2b04944fb9a54e5f9e17ed6c1b3129",
+    "url": "/tender-buttons/index.html"
+  },
+  {
+    "revision": "2937125ac37602f04362",
+    "url": "/tender-buttons/static/css/main.fea14afc.chunk.css"
+  },
+  {
+    "revision": "987e0f826482766ce8c7",
+    "url": "/tender-buttons/static/js/2.bc8259ec.chunk.js"
+  },
+  {
+    "revision": "2937125ac37602f04362",
+    "url": "/tender-buttons/static/js/main.5ae06540.chunk.js"
+  },
+  {
+    "revision": "c8e2aba7ba1303f00ed0",
+    "url": "/tender-buttons/static/js/runtime~main.0842621b.js"
+  }
+]);
